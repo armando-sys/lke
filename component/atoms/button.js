3b861eea98cb760ca4458ce64cd01a07586ccefe
armando-sys <patrickarmando87@gmail.com>
2021-05-18 17:42:04 +0900

    update

--- component/atoms/button.js
+++ component/atoms/button.js
@@ -1,3 +1,19 @@
-export default function AtomButton() {
+export default function AtomButton({ customClass, background, color, value, width, height, padding, margin, border, radius }) {
+    return (<>
+        <button style={{
+            className: customClass,
+            background: background,
+            color: color,
+            width: width,
+            height: height,
+            padding: padding,
+            margin: margin,
+            border: border,
+            borderRadius: radius,
+        }}>
+            {value}
+        </button>
 
+
+    </>);
 }
--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,13 @@
 import AtomText from "../component/atoms/paragprah";
+import AtomButton from "../component/atoms/button";
 
 export default function test() {
     return (<>
-        <AtomText value="Armando" weight="bold" size="24px" />
+        <div className="container">
+            <AtomText value="Armando" weight="bold" size="24px" />
 
-        <AtomText value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+            <AtomText value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+            <AtomButton value="Simpan" padding="5px 10px" background="blue" border="1px solid" radius="5px" />
+        </div>
     </>);
 }
